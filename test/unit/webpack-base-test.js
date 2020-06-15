9b98efac5e049728f5278ffad32e687367d349c9
taotaokang <861095174@qq.com>
2020-06-15 09:54:49 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -6,8 +6,9 @@ describe('webpack.base.js test case', () => {
   // console.log(baseConfig)
 
   // 匹配当前baseConfig输出的实际目录
+  // 'G:/mycode/webpack-code/webpack-travisCI/test/smoke/template/src/index/index.js'
   it('entry', () => {
-    assert.equal(baseConfig.entry.index, 'G:/mycode/webpack-code/webpack-travisCI/test/smoke/template/src/index/index.js');
-    assert.equal(baseConfig.entry.search, 'G:/mycode/webpack-code/webpack-travisCI/test/smoke/template/src/search/index.js');
+    assert.equal(baseConfig.entry.index.indexOf('webpack-travisCI/test/smoke/template/src/index/index.js') > -1, true);
+    assert.equal(baseConfig.entry.search.indexOf('webpack-travisCI/test/smoke/template/src/search/index.js') > -1, true);
   });
 });
